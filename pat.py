67e4352cc9f0bc9cdc40094493a92e204e31afca
adler <nkj20932@hotmail.com>
2016-10-14 23:35:49 +0800

    fix and update pat.py

--- pat.py
+++ pat.py
@@ -13,14 +13,58 @@ class Utilities:
     red_color = '\033[31m'
     blue_color = '\033[34m'
     purple_color = '\033[35m'
+
+    def extract_company_name(self, domain_name):
+        return domain_name.replace("http://", "").replace("https://", "")
+
     def __init__(self):
         self.name = 'static class'
 
+class Terminal:
+    def __init__(self, commands_dict):
+        self.commands = commands_dict
+
+    def _open_terminal(self, cmd):
+        output = "[+] Executed: {0} \r\n".format(cmd[1])
+        print("- Analyzing: {0}".format(cmd[0]))
+        if "#" in cmd[0]:
+            print("Skipped.")
+            print("")
+            return ""
+        print("- Running: {0}".format(cmd[1]))
+        # try:
+        process_stdout = subprocess.check_output(cmd[1], shell=True, stderr=subprocess.STDOUT)
+        output += str(process_stdout)
+        output += '\r\n'
+        # except Exception as e:
+        #     output += str(e)
+        output += "----------------\r\n"
+        return output
+
+    def execute(self, commands):
+        result = ''
+        for index, command in self.commands.items():
+            output = self._open_terminal(command)
+            result += output
+        return result
+
+    def save_results(self, results, folder_name, file_name):
+        if not os.path.isdir(folder_name):
+            os.mkdir(folder_name)
+        file_name = "{0}/{1}".format(folder_name, file_name)
+        file_to_save = open(file_name, 'w')
+        file_to_save.write(results)
+        file_to_save.close()
+
 class Core:
+    def __init__(self, company_domain_name, utilities):
+        self.company_domain_name = company_domain_name
+        self.utilities = utilities
+
     def _create_client_domain_folder(self):
         root_folder_path = self.utilities.reports_folder_name + "/" + self.company_domain_name
         if not os.path.isdir(root_folder_path):
-            os.mkdir(root_folder_path)
+            os.makedirs(root_folder_path)
 
     def _inject_parameter_in_file(self, line):
         line = line.replace('[domain]', self.company_domain_name)
@@ -50,25 +94,27 @@ class Core:
             websites_file = open(file_path, 'r')
             counter = 0
             for website_line in websites_file.readlines():
-                try:
-                    websites[counter] = self._inject_parameter_in_file(website_line)
-                    counter += 1
-                except Exception as e:
-                    print(self.utilities.red_color + "Error")
+                # try:
+                websites[counter] = self._inject_parameter_in_file(website_line)
+                counter += 1
+                # except Exception as e:
+                #     print(self.utilities.red_color + "Error")
         return websites
-        
+
     def _open_websites(self, websites):
-        for website in websites:
+        for index, website in websites.items():
             webbrowser.open_new_tab(website)
+            time.sleep(4)
 
     def _start(self, commands, websites, folder_name, file_name):
-        if commands == {} and websites == []:
-            results = self.execute_commands(commands)
-            print(results)
-            self._save_results(results, folder_name, file_name)
         if websites != []:
             self._open_websites(websites)
-        
+        if commands != {}:
+            terminal = Terminal(commands)
+            results = terminal.execute(commands)
+            print("Result: {0}".format(results))
+            terminal.save_results(results, folder_name, file_name)
+
     def pen_test(self, action_name, category_name):
         self._create_client_domain_folder()
         commands = self._get_commands_from_file(action_name)
@@ -80,7 +126,7 @@ class Core:
 class Main:
     def __init__(self, utilities):
         self.utilities = utilities
-        
+
     def _usage(self):
         print("Penetest Automation Tool")
         print(self.utilities.seperator_single_line)
@@ -89,9 +135,9 @@ class Main:
     def _print_banner(self, company_domain_name):
         print("Penetesting Client Domain Name " + company_domain_name)
         print(self.utilities.seperator_single_line)
-        print()
+        print("")
 
-    def _process_argument(self, args, core):
+    def _process_arguments(self, args, core):
         if args.dns_test:
             core.pen_test('dns', self.utilities.reconnaissance_category)
         if args.whois_test:
@@ -124,7 +170,7 @@ class Main:
             self._usage()
 
         return args
-        
+
     def start(self):
         args = self._initialize_arguments()
         core = Core(args.company, self.utilities)
